65b529f43db30d145657f30712c3b77b80a93aa6
Ashish Seervi <seerviashish17@gmail.com>
2020-08-08 03:14:47 +0530

    fix(server): global error fixed

--- server/src/utils/ApiError.js
+++ server/src/utils/ApiError.js
@@ -1,12 +1,20 @@
+const config = require('../config/config');
+const logger = require('../config/logger');
+
 class ApiError extends Error {
   constructor(statusCode, message, isOperational = true, stack = '') {
     super(message);
     this.statusCode = statusCode;
     this.isOperational = isOperational;
-    if (stack) {
-      this.stack = stack;
+    if (['development', 'test'].includes(config.env)) {
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+        logger.error(`ServerError: [${message}] : ${this.stack}`);
+      }
     } else {
-      Error.captureStackTrace(this, this.constructor);
+      this.stack = 'That’s all we know.';
     }
   }
 }
